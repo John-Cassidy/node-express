ed42f9d6de85b57548f9c9f3c4aeb61214203e1b
John Cassidy <jpcassidy@outlook.com>
2021-12-01 13:37:26 -0500

    add serving static index.html page

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const chalk = require('chalk');
+const path = require('path');
 
 const app = express();
 const { PORT = 3000 } = process.env;
+
+app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/', (req, res) => {
   res.send('Hello from my node-express');
