d8e04c71db13d76f0a79736168c90f8552222efe
John Cassidy <jpcassidy@outlook.com>
2021-12-01 14:52:21 -0500

    add routes for sessions

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const path = require('path');
 
 const app = express();
 const { PORT = 3000 } = process.env;
+const sessionsRouter = require('./src/routers/sessionsRouter');
 
 app.use(express.static(path.join(__dirname, '/public/')));
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
+app.use('/sessions', sessionsRouter);
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Globomantics', data: ['a', 'b', 'c'] });
