d56103d5cb70efb874eaf57a8d6ef16f9b1aced1
John Cassidy <jpcassidy@outlook.com>
2021-12-01 13:58:26 -0500

    add ejs template engine

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const { PORT = 3000 } = process.env;
 
 app.use(express.static(path.join(__dirname, '/public/')));
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Hello from my node-express');
+  res.render('index', { title: 'Globomantics', data: ['a', 'b', 'c'] });
 });
 
 app.listen(PORT, () => {
